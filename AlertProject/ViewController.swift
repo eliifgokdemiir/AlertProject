734b74997b0b0cc61185a8a74c28675face7de79
eliifgokdemiir <gokdemirelif12@gmail.com>
2024-10-30 10:41:23 +0300

    test

--- AlertProject/ViewController.swift
+++ AlertProject/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var password2Text: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var userNameText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signUpClicked(_ sender: Any) {
+        
+        if userNameText.text == "" {
+            makeAlert(titleInput: "Error!", messageInput: "Username Not found")
+        } else if passwordText.text == "" {
+            makeAlert(titleInput: "Error", messageInput: "password not found")
+        }else if passwordText.text != password2Text.text {
+            makeAlert(titleInput: "Error", messageInput: "Do not Matching Password")
+        }else {
+            makeAlert(titleInput: "Success", messageInput: "user OK")
+        }
+    }
+    
+    func makeAlert(titleInput: String, messageInput: String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
